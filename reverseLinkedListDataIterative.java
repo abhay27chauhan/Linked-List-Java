8d4ef7845033aa9724963e0abbecca4f7d1bb337
Abhay <chauhan27.abhay@gmail.com>
2020-10-08 23:18:04 +0530

    reverse linked list- data iterative

---
+++ reverseLinkedListDataIterative.java
@@ -0,0 +1,200 @@
+class reverseLinkedListDataIterative{
+
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class MyLinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val){
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if(size == 0){
+                head = tail = temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        void addFirst(int val){
+            Node temp = new Node();
+            temp.data = val;
+
+            if(size == 0){
+                temp.next = null;
+                head = tail = temp;
+            }else{
+                temp.next = head;
+                head = temp;
+            }
+            size++;
+        }
+
+        void addAt(int idx, int val){
+            if(idx<0 || idx > size){
+                System.out.println("Invalid Argument");
+            }else if(idx == 0){
+                addFirst(val);
+            }else if(idx == size){
+                addLast(val);
+            }else{
+                Node node = new Node();
+                node.data = val;
+
+                Node temp = head;
+
+                for(int i=0; i<idx-1; i++){
+                    temp = temp.next;
+                }
+
+                node.next = temp.next;
+                temp.next = node;
+                size++;
+
+
+            }
+        }
+
+        int size(){
+            return size;
+        }
+
+        void display(){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+        void removeFirst(){
+            if(size == 0){
+                System.out.println("List is Empty");
+            }else if(size ==1){
+                head = tail = null;
+                size = 0;
+            }else{
+                head = head.next;
+                size--;
+            }
+        }
+
+        void removeLast(){
+            if(size == 0){
+                System.out.println("List is Empty");
+            }else if(size == 1){
+                head = tail = null;
+                size = 0;
+            }else{
+                Node temp = head;
+                for(int i=0; i<size-2; i++){
+                    temp = temp.next;
+                }
+
+                tail = temp;
+                tail.next = null;
+                size--;
+            }
+        }
+
+        void removeAt(int idx){
+            if(size == 0){
+                System.out.println("List is Empty");
+            }else if(idx < 0 || idx >= size){
+                System.out.println("Invalid Argument");
+            }else if(idx == 0){
+                removeFirst();
+            }else if(idx == size-1){
+                removeLast();
+            }else{
+                Node temp = head;
+
+                for(int i=0; i<idx-1; i++){
+                    temp = temp.next;
+                }
+
+                temp.next = temp.next.next;
+                size--;
+            }
+        }
+
+        int getFirst(){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else{
+                return head.data;
+            }
+        }
+
+        int getLast(){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else{
+                return tail.data;
+            }
+        }
+
+        int getAt(int idx){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else if(idx<0 || idx>=size){
+                System.out.println("Invalid Argument");
+                return -1;
+            }else{
+                Node temp = head;
+                for(int i=0; i<idx; i++){
+                    temp = temp.next;
+                }
+                return temp.data;
+            }
+        }
+
+        private Node getNodeAt(int idx){
+            Node temp = head;
+            for(int i=0; i<idx; i++){
+                temp = temp.next;
+            }
+
+            return temp;
+        }
+        void reverseDI(MyLinkedList list){
+            int i =0;
+            int j = list.size()-1;
+
+            while(i<j){
+                Node left = getNodeAt(i);
+                Node right = getNodeAt(j);
+
+                int temp = left.data;
+                left.data = right.data;
+                right.data = temp;
+
+                i++;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        MyLinkedList list = new MyLinkedList();
+        for(int i=10; i<100; i += 10){
+            list.addLast(i);
+        }
+        list.display();
+        list.reverseDI(list);
+        list.display();
+    }
+}
