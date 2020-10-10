d3d44f458b1af0cb7255324fa057cde64229ef4f
Abhay <chauhan27.abhay@gmail.com>
2020-10-11 00:35:54 +0530

    get kth element from the end

---
+++ getKthElementfromEnd.java
@@ -0,0 +1,208 @@
+class getKthElementfromEnd{
+
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class MyLinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val){
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if(size == 0){
+                head = tail = temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        void addFirst(int val){
+            Node temp = new Node();
+            temp.data = val;
+
+            if(size == 0){
+                temp.next = null;
+                head = tail = temp;
+            }else{
+                temp.next = head;
+                head = temp;
+            }
+            size++;
+        }
+
+        void addAt(int idx, int val){
+            if(idx<0 || idx > size){
+                System.out.println("Invalid Argument");
+            }else if(idx == 0){
+                addFirst(val);
+            }else if(idx == size){
+                addLast(val);
+            }else{
+                Node node = new Node();
+                node.data = val;
+
+                Node temp = head;
+
+                for(int i=0; i<idx-1; i++){
+                    temp = temp.next;
+                }
+
+                node.next = temp.next;
+                temp.next = node;
+                size++;
+
+
+            }
+        }
+
+        int size(){
+            return size;
+        }
+
+        void display(){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+        void removeFirst(){
+            if(size == 0){
+                System.out.println("List is Empty");
+            }else if(size ==1){
+                head = tail = null;
+                size = 0;
+            }else{
+                head = head.next;
+                size--;
+            }
+        }
+
+        void removeLast(){
+            if(size == 0){
+                System.out.println("List is Empty");
+            }else if(size == 1){
+                head = tail = null;
+                size = 0;
+            }else{
+                Node temp = head;
+                for(int i=0; i<size-2; i++){
+                    temp = temp.next;
+                }
+
+                tail = temp;
+                tail.next = null;
+                size--;
+            }
+        }
+
+        void removeAt(int idx){
+            if(size == 0){
+                System.out.println("List is Empty");
+            }else if(idx < 0 || idx >= size){
+                System.out.println("Invalid Argument");
+            }else if(idx == 0){
+                removeFirst();
+            }else if(idx == size-1){
+                removeLast();
+            }else{
+                Node temp = head;
+
+                for(int i=0; i<idx-1; i++){
+                    temp = temp.next;
+                }
+
+                temp.next = temp.next.next;
+                size--;
+            }
+        }
+
+        int getFirst(){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else{
+                return head.data;
+            }
+        }
+
+        int getLast(){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else{
+                return tail.data;
+            }
+        }
+
+        int getAt(int idx){
+            if(size == 0){
+                System.out.println("List is Empty");
+                return -1;
+            }else if(idx<0 || idx>=size){
+                System.out.println("Invalid Argument");
+                return -1;
+            }else{
+                Node temp = head;
+                for(int i=0; i<idx; i++){
+                    temp = temp.next;
+                }
+                return temp.data;
+            }
+        }
+
+        void reversePI(MyLinkedList list){
+            Node prev = null;
+            Node curr = head;
+
+            while(curr != null){
+                Node next = curr.next;
+
+                curr.next = prev;
+
+                prev = curr;
+                curr = next;
+            }
+
+            Node temp = tail;
+            tail = head;
+            head = temp;
+            tail.next = null;
+        }
+
+        int getKthEle(int k){
+            Node s = head;
+            Node f = head;
+
+            for(int i=0; i<k; i++){
+                f = f.next;
+            }
+
+            while(f != tail){
+                s = s.next;
+                f = f.next;
+            }
+
+            return s.data;
+        }
+    }
+
+    public static void main(String[] args){
+        MyLinkedList list = new MyLinkedList();
+        for(int i=10; i<100; i += 10){
+            list.addLast(i);
+        }
+        System.out.println(list.getKthEle(2));
+    }
+}
